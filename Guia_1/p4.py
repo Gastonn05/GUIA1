5949edde27a0ff28f993d2b525f016d511932af2
Jpgenaro <71389486+JPGenaro@users.noreply.github.com>
2024-04-03 21:23:19 -0300

    Update p4.py

--- Guia_1/p4.py
+++ Guia_1/p4.py
@@ -1,3 +1,4 @@
+# Ejercicio 4
 print("Verifica si tu numero es primo o no")
 x = int(input("Escriba aquí su número: "))
 
@@ -5,6 +6,6 @@ def primo(x): #define si el numero es primo o no
     for i in range(2, x): #divide el numero por todos los numeros desde el 2, hasta todos numeros en adelante del 2 excepto el mismo numero elegido y los numeros por delante 
         if(x%i) == 0: #indica que si es igual a 0 no puede retornar como true, porq 0 no es primo :)
             return False #indica False cuando no es primo
-    return True #indica True cuando es primo       
+    return True #indica True cuando es primo
 
 print(primo(x))
