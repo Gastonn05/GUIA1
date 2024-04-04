96f23655fb6571b5646b89deff8d90be4e66daaf
Jpgenaro <71389486+JPGenaro@users.noreply.github.com>
2024-04-03 21:13:29 -0300

    Correcion

--- Guia_1/p2.py
+++ Guia_1/p2.py
@@ -1,4 +1,12 @@
+# Ejercicio 2
+x = float(input("Indique su peso en Kg: "))
+y = float(input("Indique su altura en cm: "))
 
+Resultado = x / ((y / 100) ** 2)
+
+print("Su indice de Masa Corporal es: ", Resultado)
+
+'''
 x = int(input("Indique su peso en Kg: "))
 y = int(input("Indique su altura en cm: "))
 z = y / 100
@@ -6,4 +14,4 @@ res = z * z
 
 Resultado = x / res
 
-print("Su indice de Masa Corporal es: ", Resultado)
+print("Su indice de Masa Corporal es: ", Resultado)'''
